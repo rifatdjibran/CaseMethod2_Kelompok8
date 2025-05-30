63771648187c70927ef2b990cc89bbc509261a45
mohammad zinky <mohammadzinky@gmail.com>
2025-05-30 13:31:49 +0700

    CM2

---
+++ Kendaraan.java
@@ -0,0 +1,15 @@
+public class Kendaraan {
+    String platNomor, tipe, merk;
+
+    public Kendaraan(String platNomor, String tipe, String merk) {
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Plat Nomor: " + platNomor);
+        System.out.println("Tipe: " + tipe);
+        System.out.println("Merk: " + merk);
+    }
+}
